361da345d8608a84731ef987a18842b4d71729fb
Murisi Tarusenga <murisit@gmail.com>
2026-07-27 08:59:27 +0200

    Make a simple MerkleAggregator to facilitate the testing of RecursiveAggregator.

--- aggregator/src/main.rs
+++ aggregator/src/main.rs
@@ -4,27 +4,24 @@ use std::fmt::Debug;
 use std::hash::Hash;
 use std::cmp::Ordering;
 use std::collections::BTreeMap;
-use std::collections::BinaryHeap;
-use std::cmp::Reverse;
-use std::ops::Range;
+use std::marker::PhantomData;
+use std::fmt::Display;
+use sha2::{Sha256, Digest};
+use std::ops::RangeFrom;
 use barretenberg_rs::generated_types::CircuitProveResponse;
+use proptest::prelude::*;
 
 /// Type alias to ease generic usage of aggregators
 type AggregatorBox<A> = Box<dyn Aggregator<AggregatorId = <A as Aggregator>::AggregatorId, Proof = <A as Aggregator>::Proof, BatchId = <A as Aggregator>::BatchId>>;
 
-/// Unambiguous identification of batch ID
-type QualifiedBatchId<A> = (<A as Aggregator>::AggregatorId, <A as Aggregator>::BatchId);
-
 /// The interface shared by all proof aggregators
-trait Aggregator {
+pub trait Aggregator {
     /// The type that holds aggregator IDs
     type AggregatorId;
     /// The type that holds batch IDs
     type BatchId;
     /// The type that holds proofs
     type Proof;
-    /// Push a leaf proof onto a queue of leaf proofs to aggregate
-    fn push_leaf_proof(&mut self, proof: Self::Proof);
     /// Push a batch of proofs to aggregate onto a queue
     fn push_internal_proofs(&mut self, proofs: Vec<Self::Proof>) -> Self::BatchId;
     /// Pop a recursive proof from the queue of aggregations
@@ -61,7 +58,7 @@ struct AggregatorLoad<AggregatorId> {
 
 impl<AggregatorId> AggregatorLoad<AggregatorId> {
     /// Comput load statistics from the given aggregator
-    fn new<A: Aggregator + ?Sized>(aggregator_id: AggregatorId, aggregator: &A) -> Self {
+    pub fn new<A: Aggregator + ?Sized>(aggregator_id: AggregatorId, aggregator: &A) -> Self {
         let proof_throughput = aggregator.proof_throughput();
         let expected_wait_time = if proof_throughput == 0.0 {
             f64::INFINITY
@@ -85,13 +82,9 @@ impl<AggregatorId: PartialOrd> Ord for AggregatorLoad<AggregatorId> {
 }
 
 /// Proof aggregator that works purely by delegating to workers
-struct RecursiveAggregator<AggregatorIds: Iterator, BatchIds: Iterator, Proof> {
+pub struct RecursiveAggregator<Proof, BatchIds: Iterator, AggregatorIds: Iterator> {
     /// The ID of this aggregator in its local namespace
     pub aggregator_id: AggregatorIds::Item,
-    /// Minimum batch size that can be formed from leaf proofs
-    pub leaf_batch_size: usize,
-    /// Queue of leaf proofs to be aggregated
-    pub leaf_proofs: VecDeque<Proof>,
     /// Queue of internal proofs to be aggregated
     pub internal_proofs: VecDeque<(BatchIds::Item, Vec<Proof>)>,
     /// Queue of produced recursive proofs
@@ -114,14 +107,28 @@ struct RecursiveAggregator<AggregatorIds: Iterator, BatchIds: Iterator, Proof> {
     pub proof_descendants: BTreeMap<(AggregatorIds::Item, BatchIds::Item), Vec<(AggregatorIds::Item, BatchIds::Item)>>,
 }
 
-impl<AggregatorIds: Iterator, BatchIds: Iterator, Proof> Aggregator for RecursiveAggregator<AggregatorIds, BatchIds, Proof> where AggregatorIds::Item: Hash + Eq + Copy + Debug + PartialOrd + Ord, BatchIds::Item: Hash + Eq + Copy + Ord {
+impl<Proof, BatchIds: Iterator, AggregatorIds: Iterator> RecursiveAggregator<Proof, BatchIds, AggregatorIds> {
+    pub fn new(free_batch_ids: BatchIds, mut free_aggregator_ids: AggregatorIds) -> Self {
+        Self {
+            aggregator_id: gen_id(&mut free_aggregator_ids),
+            internal_proofs: VecDeque::new(),
+            recursive_proofs: VecDeque::new(),
+            sub_recursive_proofs: HashMap::new(),
+            sub_aggregators: HashMap::new(),
+            free_aggregator_ids,
+            free_batch_ids,
+            proof_parents: HashMap::new(),
+            proof_children: HashMap::new(),
+            proof_aliases: HashMap::new(),
+            proof_descendants: BTreeMap::new(),
+        }
+    }
+}
+
+impl<AggregatorIds: Iterator, BatchIds: Iterator, Proof> Aggregator for RecursiveAggregator<Proof, BatchIds, AggregatorIds> where AggregatorIds::Item: Hash + Eq + Copy + Debug + PartialOrd + Ord + Display, BatchIds::Item: Hash + Eq + Copy + Ord + Debug {
     type AggregatorId = AggregatorIds::Item;
     type BatchId = BatchIds::Item;
     type Proof = Proof;
-    
-    fn push_leaf_proof(&mut self, proof: Self::Proof) {
-        self.leaf_proofs.push_back(proof);
-    }
 
     fn push_internal_proofs(&mut self, proofs: Vec<Self::Proof>) -> Self::BatchId {
         // Batch sizes must be powers of two
@@ -139,8 +146,7 @@ impl<AggregatorIds: Iterator, BatchIds: Iterator, Proof> Aggregator for Recursiv
 
     fn pending_queue_size(&self) -> usize {
         // Get the proofs queued in this aggregator
-        let local = self.leaf_proofs.len() +
-            self.internal_proofs.iter().map(|s| s.1.len()).sum::<usize>();
+        let local = self.internal_proofs.iter().map(|s| s.1.len()).sum::<usize>();
         // Get the proofs queued in the sub-aggregators
         let delegated: usize = self.sub_aggregators.values()
             .map(|agg| agg.pending_queue_size())
@@ -167,11 +173,8 @@ impl<AggregatorIds: Iterator, BatchIds: Iterator, Proof> Aggregator for Recursiv
     }
 
     fn step(&mut self) {
-        // If enough leaf proofs have been queued, then push them into the internaal proof queue
-        if self.leaf_proofs.len() >= self.leaf_batch_size {
-            let leaf_proofs = self.leaf_proofs.drain(..self.leaf_batch_size).collect();
-            self.push_internal_proofs(leaf_proofs);
-        }
+        println!("Distribute ------------");
+        // Compute the current aggregator loads
         let mut aggregator_ids: Vec<_> = self
             .sub_aggregators
             .iter()
@@ -215,6 +218,7 @@ impl<AggregatorIds: Iterator, BatchIds: Iterator, Proof> Aggregator for Recursiv
                     }
                     // Add the drainage to the sub aggregator
                     total_chunk_size += batch.len();
+                    println!("Sending {} proofs to aggregator {}", batch.len(), id.aggregator_id);
                     let new_batch_id = self.sub_aggregators.get_mut(&id.aggregator_id).unwrap().push_internal_proofs(batch);
                     let new_qualified_batch_id = (id.aggregator_id, new_batch_id);
                     // Replace the qualified batch ID with the new qualified batch ID
@@ -256,6 +260,7 @@ impl<AggregatorIds: Iterator, BatchIds: Iterator, Proof> Aggregator for Recursiv
                 break;
             }
         }
+        println!("Prove ------------");
         // Advance all the sub-aggregators and recombine proofs
         for (aggregator_id, aggregator) in self.sub_aggregators.iter_mut() {
             // Advance sub-aggregator
@@ -290,6 +295,7 @@ impl<AggregatorIds: Iterator, BatchIds: Iterator, Proof> Aggregator for Recursiv
                 }
             }
         }
+        println!("Consolidate ------------");
         // Finally, create new batches to be recursively proved in future calls
         self.proof_descendants.retain(|qualified_id, descendants| {
             // Recombination is only required if there are multiple descendants
@@ -301,6 +307,7 @@ impl<AggregatorIds: Iterator, BatchIds: Iterator, Proof> Aggregator for Recursiv
                     .collect();
                 // And push them back into the queue
                 assert_eq!(qualified_id.0, self.aggregator_id);
+                println!("Planning aggregation of {} proofs", proofs.len());
                 self.internal_proofs.push_back((qualified_id.1, proofs));
                 // Do not retain these descendants otherwise combination work will be duplicated.
                 // Do not even keep a singleton entry because that suggests that the merged proof
@@ -320,6 +327,179 @@ impl<AggregatorIds: Iterator, BatchIds: Iterator, Proof> Aggregator for Recursiv
     }
 }
 
+/// An aggregator that computes a Merkle root of a batch of digests.
+/// This acts as a worker node and does not delegate to further sub-aggregators.
+pub struct MerkleAggregator<BatchIds: Iterator, AggregatorIds: Iterator> {
+    /// The ID to assign to the next sub aggregator
+    pub free_aggregator_ids: PhantomData<AggregatorIds>,
+    /// The ID to assign to the next batch
+    pub free_batch_ids: BatchIds,
+    /// Queue for raw leaf proofs (though normally pushed directly to internal for testing)
+    pub leaf_queue: Vec<[u8; 32]>,
+    /// Queue of batches waiting to be merklized
+    pub internal_queue: VecDeque<(BatchIds::Item, Vec<[u8; 32]>)>,
+    /// Queue of finished Merkle roots ready to be collected
+    pub completed_proofs: VecDeque<(BatchIds::Item, [u8; 32])>,
+}
+
+impl<AggregatorIds: Iterator, BatchIds: Iterator> MerkleAggregator<BatchIds, AggregatorIds> {
+    pub fn new(free_batch_ids: BatchIds) -> Self {
+        Self {
+            free_aggregator_ids: PhantomData,
+            free_batch_ids,
+            leaf_queue: Vec::new(),
+            internal_queue: VecDeque::new(),
+            completed_proofs: VecDeque::new(),
+        }
+    }
+
+    /// A simple deterministic hash for testing purposes.
+    /// In a real scenario, this would be replaced by a cryptographic hash like SHA-256 or Poseidon.
+    fn combine_digests(left: &[u8; 32], right: &[u8; 32]) -> [u8; 32] {
+        let mut hasher = Sha256::new();
+        hasher.update(left);
+        hasher.update(right);
+        hasher.finalize().into()
+    }
+}
+
+impl<AggregatorIds: Iterator, BatchIds: Iterator> Aggregator for MerkleAggregator<BatchIds, AggregatorIds> where BatchIds::Item: Copy {
+    type AggregatorId = AggregatorIds::Item;
+    type BatchId = BatchIds::Item;
+    type Proof = [u8; 32];
+
+    fn push_internal_proofs(&mut self, proofs: Vec<Self::Proof>) -> Self::BatchId {
+        assert!(proofs.len().is_power_of_two(), "Merkle tree requires power-of-two leaves");
+        assert!(proofs.len() > 1, "Must have more than one proof to aggregate");
+        
+        let batch_id = gen_id(&mut self.free_batch_ids);
+        self.internal_queue.push_back((batch_id, proofs));
+        
+        batch_id
+    }
+
+    fn pop_recursive_proof(&mut self) -> Option<(Self::BatchId, Self::Proof)> {
+        self.completed_proofs.pop_front()
+    }
+
+    fn pending_queue_size(&self) -> usize {
+        self.leaf_queue.len()
+            + self.internal_queue.iter().map(|(_, batch)| batch.len()).sum::<usize>()
+    }
+
+    fn proof_throughput(&self) -> f64 {
+        // Returns 1.0 since this aggregator processes synchronously without parallelism
+        1.0
+    }
+
+    fn insert_sub_aggregator(&mut self, _sub_aggregator: AggregatorBox<Self>) -> Self::AggregatorId {
+        unimplemented!("MerkleAggregator is a leaf worker and does not support sub-aggregators.");
+    }
+
+    fn remove_sub_aggregator(&mut self, _id: &Self::AggregatorId) -> Option<AggregatorBox<Self>> {
+        unimplemented!("MerkleAggregator is a leaf worker and does not support sub-aggregators.");
+    }
+
+    fn step(&mut self) {
+        // Pop one batch from the internal queue to process in this step
+        while let Some((batch_id, mut current_layer)) = self.internal_queue.pop_front() {
+            println!("Aggregating {} proofs", current_layer.len());
+            // Iteratively compute the Merkle root
+            while current_layer.len() > 1 {
+                let mut next_layer = Vec::with_capacity(current_layer.len() / 2);
+                
+                for chunk in current_layer.chunks_exact(2) {
+                    let left = &chunk[0];
+                    let right = &chunk[1];
+                    next_layer.push(Self::combine_digests(left, right));
+                }
+                
+                current_layer = next_layer;
+            }
+            
+            // The single remaining element is the root
+            let root = current_layer.pop().expect("Layer should contain exactly one root digest");
+            self.completed_proofs.push_back((batch_id, root));
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut aggregator: MerkleAggregator<RangeFrom<usize>, RangeFrom<usize>> = MerkleAggregator::new(0usize..);
+    aggregator.push_internal_proofs(vec![[0; 32], [1; 32]]);
+    aggregator.step();
+    println!("Recursive proof: {:?}", aggregator.pop_recursive_proof());
+}
+
+proptest! {
+    #[test]
+    fn test_balanced_aggregator(batch: [[u8; 32]; 256]) {
+        type MerkleAggregatorT = MerkleAggregator<RangeFrom<usize>, RangeFrom<usize>>;
+        type RecursiveAggregatorT = RecursiveAggregator<[u8; 32], RangeFrom<usize>, RangeFrom<usize>>;
+        // Make a simple aggregator that directly computes the root
+        let mut merkle_aggregator = MerkleAggregatorT::new(0usize..);
+        // Push the random batch onto the aggregator
+        merkle_aggregator.push_internal_proofs(batch.to_vec());
+        // Make the aggregator process the proofs in the queue
+        merkle_aggregator.step();
+        // Make a more complex aggregator
+        let mut recursive_aggregator = RecursiveAggregatorT::new(0usize.., 0usize..);
+        // Push 4 sub-aggregators to actually handle the computations
+        recursive_aggregator.insert_sub_aggregator(Box::new(MerkleAggregatorT::new(0usize..)));
+        recursive_aggregator.insert_sub_aggregator(Box::new(MerkleAggregatorT::new(0usize..)));
+        recursive_aggregator.insert_sub_aggregator(Box::new(MerkleAggregatorT::new(0usize..)));
+        recursive_aggregator.insert_sub_aggregator(Box::new(MerkleAggregatorT::new(0usize..)));
+        // Push some work onto the recursive aggregator
+        recursive_aggregator.push_internal_proofs(batch.to_vec());
+        // Repeatedly step through distribution and consolidation
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        // Finally, ensure that the Merkle roots agree
+        assert_eq!(merkle_aggregator.pop_recursive_proof(), recursive_aggregator.pop_recursive_proof());
+        assert_eq!(merkle_aggregator.pop_recursive_proof(), None);
+        assert_eq!(recursive_aggregator.pop_recursive_proof(), None);
+    }
+
+    #[test]
+    fn test_imbalanced_aggregator(batch: [[u8; 32]; 256]) {
+        type MerkleAggregatorT = MerkleAggregator<RangeFrom<usize>, RangeFrom<usize>>;
+        type RecursiveAggregatorT = RecursiveAggregator<[u8; 32], RangeFrom<usize>, RangeFrom<usize>>;
+        // Make a simple aggregator that directly computes the root
+        let mut merkle_aggregator = MerkleAggregatorT::new(0usize..);
+        // Push the random batch onto the aggregator
+        merkle_aggregator.push_internal_proofs(batch.to_vec());
+        // Make the aggregator process the proofs in the queue
+        merkle_aggregator.step();
+        // Make a more complex aggregator
+        let mut recursive_aggregator = RecursiveAggregatorT::new(0usize.., 0usize..);
+        // Push 2 sub-aggregators to handle the computations
+        recursive_aggregator.insert_sub_aggregator(Box::new(MerkleAggregatorT::new(0usize..)));
+        // Let one of the sub-aggregators itself be a recursive aggregator
+        let mut recursive_sub_aggregator = RecursiveAggregatorT::new(0usize.., 0usize..);
+        recursive_sub_aggregator.insert_sub_aggregator(Box::new(MerkleAggregatorT::new(0usize..)));
+        recursive_sub_aggregator.insert_sub_aggregator(Box::new(MerkleAggregatorT::new(0usize..)));
+        recursive_sub_aggregator.insert_sub_aggregator(Box::new(MerkleAggregatorT::new(0usize..)));
+        recursive_aggregator.insert_sub_aggregator(Box::new(recursive_sub_aggregator));
+        // Push some work onto the recursive aggregator
+        recursive_aggregator.push_internal_proofs(batch.to_vec());
+        // Repeatedly step through distribution and consolidation
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        recursive_aggregator.step();
+        // Finally, ensure that the Merkle roots agree
+        assert_eq!(merkle_aggregator.pop_recursive_proof(), recursive_aggregator.pop_recursive_proof());
+        assert_eq!(merkle_aggregator.pop_recursive_proof(), None);
+        assert_eq!(recursive_aggregator.pop_recursive_proof(), None);
+    }
 }
